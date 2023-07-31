37938df824c8fc112fe7fead292903e382636ec4
Chris Williams <williamscd.88@googlemail.com>
2023-07-31 15:47:10 +0100

    edit qm file reading in qm2ml.py for consistency with opt output

--- qm2ml.py
+++ qm2ml.py
@@ -29,32 +29,34 @@ def gau2ml(set_size, input_dir, output_dir):
             # extract atomic coordinates
             if "Input orientation:" in line:
                 coord_block = list(islice(qm_file, 4+n_atom))[-n_atom:]
-                for i_atom, atom in enumerate(coord_block):
-                    coord[i_file, i_atom] = atom.strip('\n').split()[-3:]
-                    print(*coord[i_file, i_atom], file=coord_file)
             # extract energies
             if "SCF Done:" in line:
                 energies[i_file] = (float(line.split()[4]))
-                # convert to kcal/mol and print to energy.txt file
-                print(energies[i_file]*627.509608, file=energy_file)
             # extract forces
             if "Axes restored to original set" in line:
                 force_block = list(islice(qm_file, 4+n_atom))[-n_atom:]
-                for i_atom, atom in enumerate(force_block):
-                    # convert to kcal/mol/Angstrom and print to force.txt file
-                    force[i_file, i_atom] = atom.strip('\n').split()[-3:]
-                    print(*force[i_file, i_atom]*627.509608/0.529177,
-                          file=force_file)
             # extract charges
             if "ESP charges:" in line:
                 charge_block = list(islice(qm_file, 1+n_atom))[-n_atom:]
-                for i_atom, atom in enumerate(charge_block):
-                    charge[i_file, i_atom] = atom.strip('\n').split()[-1]
-                    print(charge[i_file, i_atom], file=charge_file)
             # assess termination state
             if "Normal termination of Gaussian 09" in line:
                 normal_term = True
                 break
+        # save coordinates to file and
+        for i_atom, atom in enumerate(coord_block):
+            coord[i_file, i_atom] = atom.strip('\n').split()[-3:]
+            print(*coord[i_file, i_atom], file=coord_file)
+        # convert to kcal/mol and print to energy.txt file
+        print(energies[i_file] * 627.509608, file=energy_file)
+        for i_atom, atom in enumerate(force_block):
+            # convert to kcal/mol/Angstrom and print to force.txt file
+            force[i_file, i_atom] = atom.strip('\n').split()[-3:]
+            print(*force[i_file, i_atom]*627.509608/0.529177,
+            file=force_file)
+        # optional reading of charges
+        #for i_atom, atom in enumerate(charge_block):
+        #    charge[i_file, i_atom] = atom.strip('\n').split()[-1]
+        #    print(charge[i_file, i_atom], file=charge_file)
         if not normal_term:
             errors.append(i_file)
             print(i_file, file=error_file)
